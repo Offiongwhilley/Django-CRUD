a20ee8066faf29a53a3fc89de3f9c445ff67e6e4
Offiongwhilley <offiongwhilley@gmail.com>
2022-06-24 09:17:27 +0100

    adding project files

---
+++ I4G005249YDA/blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(title)
+admin.site.register(slug)
+admin.site.register(author)
+admin.site.register(body)
+admin.site.register(published)
+admin.site.register(created)
+admin.site.register(updated)
+admin.site.register(status)
+
+
+
+
